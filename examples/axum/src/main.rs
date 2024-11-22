7a60607f61bbaf2c1ca5a5ad60cba9c25a6e8ddd
Uwe Nanninga <uwe.nanninga@gmail.com>
2024-11-22 14:17:53 +0100

    adds request language configuration to axum example

--- examples/axum/src/main.rs
+++ examples/axum/src/main.rs
@@ -1,5 +1,5 @@
 use axum::{routing::get, Router};
-use fluent_static::support::axum::RequestLanguage;
+use fluent_static::support::axum::{RequestLanguage, RequestLanguageConfig};
 use maud::{html, Markup};
 
 mod l10n {
@@ -15,7 +15,27 @@ mod l10n {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    // By default, only the `Accept-Language` HTTP header is used to determine the
+    // client's preferred language.
+    // The `RequestLanguageConfig` struct allows you to configure the language selection.
+    // This includes
+    // - `ignore_accept_language_header`,
+    // - `language_cookie_name` and
+    // - `fallback_language_id`.
+
+    // In the following configuration, the cookie name is set to `"lang"`.
+    // If this cookie is present, the value will be used instead of the `Accept-Language` header.
+    let request_lang_config = RequestLanguageConfig::builder()
+        .language_cookie_name("lang")
+        .build();
+
+    let app = Router::new()
+        .route("/", get(handler))
+        // The configuration must then be set as an axum request extension,
+        // which will apply it to all routes.
+        // See https://docs.rs/axum/latest/axum/#using-request-extensions
+        .layer(axum::Extension(request_lang_config));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
